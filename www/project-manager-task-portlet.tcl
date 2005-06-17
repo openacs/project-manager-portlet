6fc8df734caa40c53809b16e25b77efdd7b0959b
Malte Sussdorff <openacs@sussdorff.de>
2005-06-17 09:26:59 +0000

    Fixed task link issue

--- www/project-manager-task-portlet.tcl
+++ www/project-manager-task-portlet.tcl
@@ -1,6 +1,14 @@
 array set config $cf
 
-set base_url "[ad_conn package_url]project-manager"
+set community_id [dotlrn_community::get_community_id_from_url \
+		  -url [ad_conn url] \
+		     ]
+
+if {![empty_string_p $community_id]} {
+
+    set base_url "project-manager/"
+
+}
 
 set package_id $config(project_manager_id)
 
