aea736e5412bc9bb23fc3f199ead0761c4837979
Malte Sussdorff <openacs@sussdorff.de>
2005-07-31 22:24:00 +0000

    Added correct portlet name

--- sql/postgresql/project-manager-portlet-create.sql
+++ sql/postgresql/project-manager-portlet-create.sql
@@ -273,4 +273,4 @@ drop function inline_0();
 \i project-manager-task-portlet-create.sql
 \i project-manager-admin-portlet-create.sql
 \i project-manager-calendar-portlet-create.sql
-\i project-manager-project-calendar-portlet-create.sql
+\i project-manager-project-calendar-portlet.sql
